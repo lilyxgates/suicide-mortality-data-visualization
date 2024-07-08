3388856d78a9d8629c155ae32186188686351af5
Lily Gates <lilgates@sky.atmos.umd.edu>
2024-07-08 17:39:19 -0400

    Added 5-yr running mean, deleted ages graph

--- final_scatter.py
+++ final_scatter.py
@@ -95,17 +95,48 @@ max_m_sex = max(ss_sexes["MALE"][1])
 min_m_sex = min(ss_sexes["MALE"][1])
 
 
-# Plotting Sexes Data
+# PLOTTING DATA
 # SEXES: Total, All Sexes
 ax1.scatter(x=ss_sexes["TOTAL"][0], y=ss_sexes["TOTAL"][1], c='k', alpha=0.3, label="Total")
 ax1.scatter(x=ss_sexes["FEMALE"][0], y=ss_sexes["FEMALE"][1], c='crimson', alpha=0.3, label="Female")
 ax1.scatter(x=ss_sexes["MALE"][0], y=ss_sexes["MALE"][1], c='blue', alpha=0.3, label="Male")
-
 # Individual
 ax2.scatter(x=ss_sexes["TOTAL"][0], y=ss_sexes["TOTAL"][1], c='k', alpha=0.3, label="Total")
 ax3.scatter(x=ss_sexes["FEMALE"][0], y=ss_sexes["FEMALE"][1], c='crimson', alpha=0.3, label="Female")
 ax4.scatter(x=ss_sexes["MALE"][0], y=ss_sexes["MALE"][1], c='blue', alpha=0.3, label="Male")
 
+
+# RUNNING MEAN AND LINEAR REGRESSION (PREDICTION)
+# Running Mean (5 Year)
+## Generate Running Mean
+rm_rate = rm.running_mean((ss_sexes["TOTAL"][1]), 5)  # Using the Prof. function
+# Cannot do running mean on first and last 2 values, removes the 0 tails
+ax1.plot(ss_sexes["TOTAL"][0][2:-2], ss_sexes["TOTAL"][1][2:-2], c='rebeccapurple', label='5-yr Running Mean') 
+
+"""
+# Linear Fit
+## Generate Linear Fit Coef
+coef_lin = np.polyfit(year, temp, 1)  # x value, y value, 1st order
+## Generate Linear Fit Formula
+lf = np.poly1d(coef_lin)
+## Need New X Values
+xfit = np.linspace(year[0], year[-1], 200)
+## Usig lf function Generate new y values
+yfit_1 = lf(xfit)
+
+plt.plot(xfit, yfit_1, color='red', label='Linear Fit')  # Red, line, labeled
+print("Linear Fit Formula:")
+print(lf)
+
+# FUTURE PROJECTIONS -- from 2023 to 2050 (27 years)
+# Linear Fit (to 2050)
+xfit_1f = np.linspace(year[0], year[-1]+27, 200)
+yfit_1f = lf(xfit_1f)
+plt.plot(xfit_1f, yfit_1f, ls='--', color='red', label='Linear Fit Projection to 2050')  # Red, dashed line, labeled
+"""
+
+
+# TITLES AND AXIS LABELS
 # Legend
 #ax1.legend(loc='upper center', bbox_to_anchor=(0.5, 0.2), fancybox=True, shadow=True, ncol=7)
 ax1.legend(loc=0)
@@ -127,173 +158,6 @@ fig1.suptitle('Comparing Annual Suicide Mortality Rates (2000-2019)\nBy Sexes', 
 fig1.text(0.5, 0.025, "Source: World Health Organization 2024 data.who.int, Suicide mortality rate (per 100 000 population) [Indicator]. https://data.who.int/indicators/i/F08B4FD/16BBF41 (Accessed on 1 July 2024)\nVisualized by Lily Gates for AOSC247 at the University of Maryland", fontsize=7, ha='center')
 
 
-
-
 plt.show()
 
-######################################
-################ AGES ################
-######################################
 
-# SAVE AGES DATA
-ages = ["TOTAL", "Y15T24", "Y25T34", "Y35T44", "Y45T54", "Y55T64", "Y65T74", "Y75T84", "Y_GE85"]
-
-ss_ages = {}
-for age in ages:
-    s_age = data_original[(data_original["DIM_AGE"] == age)][["DIM_TIME", "RATE_PER_100000_N"]].groupby(["DIM_TIME"]).mean()
-    print(f"{age}:")
-    print(s_age)
-    ss_ages[age] = [s_age.index.to_numpy(), s_age.values]
-
-# SETTING UP THE FIGURE/PLOTS -- AGES
-fig2 = plt.figure(figsize=[16,12])
-ax1 = plt.subplot2grid((15,2), (0,0), rowspan=4, colspan=2)  # TOTAL, ALL AGES
-#plt.setp(ax2.get_xticklabels(), visible=False) 
-ax2 = plt.subplot2grid((15,2), (5,0), rowspan=2)  # Y15T24
-ax3 = plt.subplot2grid((15,2), (8,0), rowspan=2)  # Y25T34
-ax4 = plt.subplot2grid((15,2), (11,0), rowspan=2)  # Y35T44
-ax5 = plt.subplot2grid((15,2), (14,0), rowspan=2)  # Y45T54
-ax6 = plt.subplot2grid((15,2), (5,1), rowspan=2)  # Y55T64
-ax7 = plt.subplot2grid((15,2), (8,1), rowspan=2)  # Y65T74
-ax8 = plt.subplot2grid((15,2), (11,1), rowspan=2)  # Y75T84
-ax9 = plt.subplot2grid((15,2), (14,1), rowspan=2)  # Y_GE85
-
-
-
-# PLOTTING  DATA
-# Setting Axis Limits
-#ymin = 5
-#ymax = 25
-
-# Finding Max and Min Values for Rates
-# Ages
-max_total_age = max(ss_ages["TOTAL"][1])
-min_total_age = min(ss_ages["TOTAL"][1])
-
-max_15 = max(ss_ages["Y15T24"][1])
-min_15 = min(ss_ages["Y15T24"][1])
-
-max_25 = max(ss_ages["Y25T34"][1])
-min_25 = min(ss_ages["Y25T34"][1])
-
-max_35 = max(ss_ages["Y35T44"][1])
-min_35 = min(ss_ages["Y35T44"][1])
-
-max_45 = max(ss_ages["Y45T54"][1])
-min_45 = min(ss_ages["Y45T54"][1])
-
-max_55 = max(ss_ages["Y55T64"][1])
-min_55 = min(ss_ages["Y55T64"][1])
-
-max_65 = max(ss_ages["Y65T74"][1])
-min_65 = min(ss_ages["Y65T74"][1])
-
-max_75 = max(ss_ages["Y75T84"][1])
-min_75 = min(ss_ages["Y75T84"][1])
-
-max_85 = max(ss_ages["Y_GE85"][1])
-min_85 = min(ss_ages["Y_GE85"][1])
-
-
-print(min_total_age)
-print(max_total_age)
-
-print(min_15)
-print(max_15)
-
-print(min_25)
-print(max_25)
-
-print(min_35)
-print(max_35)
-
-print(min_45)
-print(max_45)
-
-print(min_55)
-print(max_55)
-
-print(min_65)
-print(max_65)
-
-print(min_75)
-print(max_75)
-
-print(min_85)
-print(max_85)
-
-
-# Set Axis Ranges
-ax1.set_ylim(0, 32)
-ax2.set_ylim(min_15, max_15)
-ax3.set_ylim(min_25, max_25)
-ax4.set_ylim(min_35, max_35)
-ax5.set_ylim(min_45, max_45)
-ax6.set_ylim(min_55, max_55)
-ax7.set_ylim(min_65, max_65)
-ax8.set_ylim(min_75, max_75)
-ax9.set_ylim(min_85, max_85)
-
-# AGES: Total, All Ages
-ax1.scatter(x=ss_ages["TOTAL"][0], y=ss_ages["TOTAL"][1], c='k', alpha=0.3, label="Total")
-ax1.scatter(x=ss_ages["Y15T24"][0], y=ss_ages["Y15T24"][1], c='crimson', alpha=0.3, label="Ages 15-24")
-ax1.scatter(x=ss_ages["Y25T34"][0], y=ss_ages["Y25T34"][1], c='peru', alpha=0.3, label="Ages 25-34")
-ax1.scatter(x=ss_ages["Y35T44"][0], y=ss_ages["Y35T44"][1], c='goldenrod', alpha=0.3, label="Ages 35-44")
-ax1.scatter(x=ss_ages["Y45T54"][0], y=ss_ages["Y45T54"][1], c='yellowgreen', alpha=0.3, label="Ages 45-54")
-ax1.scatter(x=ss_ages["Y55T64"][0], y=ss_ages["Y55T64"][1], c='olivedrab', alpha=0.3, label="Ages 55-64")
-ax1.scatter(x=ss_ages["Y65T74"][0], y=ss_ages["Y65T74"][1], c='darkcyan', alpha=0.3, label="Ages 65-74")
-ax1.scatter(x=ss_ages["Y75T84"][0], y=ss_ages["Y75T84"][1], c='mediumblue', alpha=0.3, label="Ages 75-84")
-ax1.scatter(x=ss_ages["Y_GE85"][0], y=ss_ages["Y_GE85"][1], c='purple', alpha=0.3, label="Ages 85+")
-
-
-# Individual
-ax2.scatter(x=ss_ages["Y15T24"][0], y=ss_ages["Y15T24"][1], c='crimson', alpha=0.3, label="Ages 15-24")
-ax3.scatter(x=ss_ages["Y25T34"][0], y=ss_ages["Y25T34"][1], c='peru', alpha=0.3, label="Ages 25-34")
-ax4.scatter(x=ss_ages["Y35T44"][0], y=ss_ages["Y35T44"][1], c='goldenrod', alpha=0.3, label="Ages 35-44")
-ax5.scatter(x=ss_ages["Y45T54"][0], y=ss_ages["Y45T54"][1], c='yellowgreen', alpha=0.3, label="Ages 45-54")
-ax6.scatter(x=ss_ages["Y55T64"][0], y=ss_ages["Y55T64"][1], c='olivedrab', alpha=0.3, label="Ages 55-64")
-ax7.scatter(x=ss_ages["Y65T74"][0], y=ss_ages["Y65T74"][1], c='darkcyan', alpha=0.3, label="Ages 65-74")
-ax8.scatter(x=ss_ages["Y75T84"][0], y=ss_ages["Y75T84"][1], c='mediumblue', alpha=0.3, label="Ages 75-84")
-ax9.scatter(x=ss_ages["Y_GE85"][0], y=ss_ages["Y_GE85"][1], c='purple', alpha=0.3, label="Ages 85+")
-
-# TITLES AND AXIS LABELS
-#Figure Title
-fig2.suptitle('Comparing Annual Suicide Mortality Rates (2000-2019)\nBy Age Group', fontsize=16, ha='center')
-# Footer
-fig2.text(0.5, 0.025, "Source: World Health Organization 2024 data.who.int, Suicide mortality rate (per 100 000 population) [Indicator]. https://data.who.int/indicators/i/F08B4FD/16BBF41 (Accessed on 1 July 2024)\nVisualized by Lily Gates for AOSC247 at the University of Maryland", fontsize=7, ha='center')
-
-# X-Axis
-plt.xticks(np.arange(2000, 2020, step=1))
-
-xticks = np.arange(2000, 2020, 1)
-xlabels = ["'00", "'01", "'02", "'03", "'04", "'05", "'06", "'07", "'08", "'09", "'10", "'11", "'12", "'13", "'14", "'15", "'16", "'17", "'18", "'19"]
-
-ax1.set_xticks(xticks, labels=xticks)
-ax1.set_xlabel('Years')
-
-ax2.set_xticks(xticks, labels=xlabels)
-ax3.set_xticks(xticks, labels=xlabels)
-ax4.set_xticks(xticks, labels=xlabels)
-
-
-
-
-plt.show()
-
-"""
-
-#ax1.plot(ss_regions["World"], c='k', linestyle = "--", linewidth = '2', label="Global")  # World
-plt.plot(ss_sexes["TOTAL"], c='k', o, label="Total")  # TOTAL
-
-# Plot XY original data
-plt.plot(season, snow, 'o', label='Original Data') 
-
-# Running mean 5 year
-## Generate Running Mean
-# Using the Prof. function
-rmsnow = rm.running_mean(snow, 5) 
-# Cannot do running mean on first and last 2 values, removes the 0 tales
-plt.plot(season[2:-2], rmsnow[2:-2], label='5-yr Running Mean')
-
-
-"""
